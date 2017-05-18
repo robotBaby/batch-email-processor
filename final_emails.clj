57388502f8d994599f40042219e3bfd2aee6ecd3
Arindrima Datta <arindrimadatta@gmail.com>
2017-05-18 15:27:55 -0400

    Refactoring the monolithic code to smaller functions.

--- final_emails.clj
+++ final_emails.clj
@@ -50,64 +50,53 @@
 		email-address (nth duplicate 0)]
 		(assoc coll (nth duplicate 0) min_spam-score)))
 
+(defn compute-new-last-hundred
+	[last_hundred record]
+	(let [new-last-hundred (if (< (count last_hundred) 100) 
+		  							(conj last_hundred record) 
+		  							(conj (rest last_hundred) record)) ;; dropping the oldest of the 100 record window is full
+		  		  sum-last-hundred (reduce + (map :spam-score new-last-hundred))
+		  		  last-hundred-mean (float (/ sum-last-hundred (count new-last-hundred)))]
+	{:new-last-hundred new-last-hundred :last-hundred-mean last-hundred-mean}))
+
+
+(defn print-statictics
+	[N processed new-mean last_hundred_mean]
+	(if (= 0 (rem N 10000))
+	  ;; Printing out some fun statistics for every 10000 record
+	(do 
+	;;	(println "Processed ", processed, " Added ", N, " time ", (System/currentTimeMillis))))
+		(println "Processed ", processed, "email & Added ", N, " emails at ", (get-time))
+		(print "Last 100 mean: ", last_hundred_mean, "Current mean: ", new-mean, "\n"))))
+
+(defn check-duplicates
+	[{coll :coll seen :seen duplicates :duplicates N :N processed :processed :as old} 
+		{email-address :email-address spam-score :spam-score :as record}
+		new-mean
+		new_last_hundred]
+	(if (contains? duplicates email-address)
+	;; We we know that this is one of the duplicates, check if it has been added before
+	(if (not (contains? seen email-address))
+		;; If not seen before add the record with the lowest spam score.
+		;; This strategy helps us send more email
+  	(let [new_record (assoc record :spam-score (get duplicates email-address))]
+  		{:coll (conj coll new_record) :mean new-mean  :seen (conj seen email-address)  :duplicates duplicates :N (inc N)  :processed (inc processed):last_hundred new_last_hundred})
+  	(assoc old :processed (inc processed)))	
+		{:coll (conj coll record) :mean new-mean :seen seen :duplicates duplicates :N (inc N) :processed (inc processed):last_hundred new_last_hundred}))
 
 (defn curate_emails
-	[{coll :coll 
-		mean :mean 
-		seen :seen 
-		duplicates :duplicates 
-		N :N 
-		processed :processed 
-		last_hundred :last_hundred :as old} record]
-	(let [email-address (:email-address record)
-		  spam-score (:spam-score record)
-		  new-mean (float (/ (+ spam-score (* mean N)) (inc N)))]
+	[{coll :coll mean :mean seen :seen duplicates :duplicates N :N processed :processed last_hundred :last_hundred :as old} {email-address :email-address spam-score :spam-score :as record}]
+	(let [new-mean (float (/ (+ spam-score (* mean N)) (inc N)))]
 		  ;; The multiple if statements are meant to 
 		  ;; speed up the computation by dropping records that fail initial criteria 
 		  (if (and (< spam-score 0.3) (< new-mean 0.05))
-		  	(let [new_last_hundred (if (< (count last_hundred) 100) 
-		  							(conj last_hundred record) 
-		  							(conj (rest last_hundred) record)) ;; dropping the oldest of the 100 record window is full
-		  		  sum_last_hundred (reduce + (map :spam-score new_last_hundred))
-		  		  last_hundred_mean (float (/ sum_last_hundred (count new_last_hundred)))]
-		  		(if (< last_hundred_mean 0.1)
+		  	(let [{last-hundred-mean :last-hundred-mean new-last-hundred :new-last-hundred} (compute-new-last-hundred last_hundred record)]
+		  		(if (< last-hundred-mean 0.1)
 		  			;; If we don't know this is a duplicate, we add it if the previous conditions passed
-		  			(if (contains? duplicates email-address)
-		  				;; We we know that this is one of the duplicates, check if it has been added before
-		  				(if (not (contains? seen email-address))
-		  					;; If not seen before add the record with the lowest spam score.
-		  					;; This strategy helps us send more email
-						  	(let [new_record (assoc record :spam-score (get duplicates email-address))]
-						  		  ;; Printing out some fun statistics for every 10000 record
-						  		  (if (= 0 (rem N 10000))
-						  		  	(do 
-						  		  	;;	(println "Processed ", processed, " Added ", N, " time ", (System/currentTimeMillis))))
-			  							(println "Processed ", processed, "email & Added ", N, " emails at ", (get-time))
-			  							(print "Last 100 mean: ", last_hundred_mean, "Current mean: ", new-mean, "\n")))
-						  		  {:coll (conj coll new_record) 
-						  		  	:mean new-mean 
-						  		  	:seen (conj seen email-address) 
-						  		  	:duplicates duplicates 
-						  		  	:N (inc N) 
-						  		  	:processed (inc processed)
-						  		  	:last_hundred new_last_hundred})
-						  	(assoc old :processed (inc processed)))
-		  				(do 
-		  					(if (= 0 (rem N 10000))
-						  		  ;; Printing out some fun statistics for every 10000 record
-		  						(do 
-		  						;;	(println "Processed ", processed, " Added ", N, " time ", (System/currentTimeMillis))))
-			  						(println "Processed ", processed, "email & Added ", N, " emails at ", (get-time))
-			  						(print "Last 100 mean: ", last_hundred_mean, "Current mean: ", new-mean, "\n")))
-		  					;; If not a duplicate, add it away!
-					  		{:coll (conj coll record) 
-					  			:mean new-mean 
-					  			:seen seen 
-					  			:duplicates duplicates 
-					  			:N (inc N) 
-					  			:processed (inc processed)
-					  			:last_hundred new_last_hundred}))
-		  	(assoc old :processed (inc processed))))
+		  			(do
+		  				(print-statictics N processed new-mean last-hundred-mean)
+		  				(check-duplicates old record new-mean new-last-hundred))
+		  (assoc old :processed (inc processed))))
 		  (assoc old :processed (inc processed)))))
 
 
@@ -144,7 +133,7 @@
 						{:email-address "cd.com" :spam-score 0.01}]
 		duplicate_list (dups-with-function all_records :email-address)
 		duplicates (reduce create_duplicate_map {} duplicate_list)
-		result (reduce curate_emails {:coll [] :seen #{} :mean 0 :duplicates duplicates :N 0 :processed 0 :last_hundred []} all_records)]
+		result (reduce curate_emails {:coll [] :seen #{} :mean 0 :duplicates duplicates :N 0 :processed 0 :last_hundred [] :last_hundred_mean 0} all_records)]
 	(is (= duplicates {"ab.com" 0.03}))
 	(is (= (:seen result) #{"ab.com"}))
 	(is (= (:coll result) [{:email-address "ab.com" :spam-score 0.03}, {:email-address "cd.com" :spam-score 0.01}]))))
